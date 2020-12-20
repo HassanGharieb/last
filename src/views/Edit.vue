31bcea281f00519857a20358f0d1181071bbed45
Hassan Gharieb <hassangharieb4@gmail.com>
2020-12-20 13:29:52 +0200

    'edit'

---
+++ src/views/Edit.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="">
+
+<h1>This is an edit page</h1>
+    </div>
+</template>
---
+++ src/views/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list">
+    {{ this.$route.params.classs }}
+    <button type="submit" value="submit" @click="edit">Edit</button>
+  </div>
+</template>
+<script>
+export default {
+    name: "List",
+  data: function () {
+   
+    return {
+      say: this.$route.params.classs,
+    };
+  },
+  methods: {
+    edit: function () {
+      this.calc.id = 1;
+      this.$router.push({
+        name: "Edit",
+        params: { class: this.calc, id: this.calc.id },
+      });
+    },
+  },
+  computed: {
+    calc: function () {
+      return this.$route.params.classs;
+    },
+  },
+};
+</script>
